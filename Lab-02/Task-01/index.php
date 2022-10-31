37cd9d092f5f7ad07b6687de5fe5d985a258a669
limanp <pasha.liman@ukr.net>
2022-10-31 15:17:54 +0200

    refactoring lab-02 task-01

--- Lab-02/Task-01/index.php
+++ Lab-02/Task-01/index.php
@@ -4,55 +4,60 @@
         table {
             border-collapse: collapse;
         }
+
         table, th, td {
             border: solid 1px black;
         }
-        tr:first-child{
+
+        tr:first-child {
             background-color: yellow;
         }
+
         form {
             display: flex;
         }
+
         input[type="submit"] {
             width: 100px;
         }
+
         input {
             margin: 10px;
         }
     </style>
 </head>
 <body>
-    <form >
-        <table>
-            <tr>
-                <th>x^y</th>
-                <th>x!</th>
-                <th>my_tg(x)</th>
-                <th>sin(x)</th>
-                <th>cos(x)</th>
-                <th>tg(x)</th>
-                <th>x+y</th>
-                <th>x-y</th>
-                <th>x*y</th>
-                <th>x/y</th>
-                <th>average(x,y)</th>
-            </tr>
-            <tr>
-                <?php
+<form>
+    <table>
+        <tr>
+            <th>x^y</th>
+            <th>x!</th>
+            <th>my_tg(x)</th>
+            <th>sin(x)</th>
+            <th>cos(x)</th>
+            <th>tg(x)</th>
+            <th>x+y</th>
+            <th>x-y</th>
+            <th>x*y</th>
+            <th>x/y</th>
+            <th>average(x,y)</th>
+        </tr>
+        <tr>
+            <?php
 
-                ?>
-            </tr>
-        </table>
-        <div>
-            <input type="number" name="numX" >
-        </div>
-        <div>
-            <input type="number" name="numY" >
-        </div>
-        <div>
-            <input type="submit" value="=" >
-        </div>
-    </form>
+            ?>
+        </tr>
+    </table>
+    <div>
+        <input type="number" name="numX">
+    </div>
+    <div>
+        <input type="number" name="numY">
+    </div>
+    <div>
+        <input type="submit" value="=">
+    </div>
+</form>
 </body>
 </html>
 
