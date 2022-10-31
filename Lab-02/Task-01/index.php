ced380c397a4ef10863664a17fa82039cc86403e
limanp <pasha.liman@ukr.net>
2022-10-31 14:24:25 +0200

    created form

--- Lab-02/Task-01/index.php
+++ Lab-02/Task-01/index.php
@@ -1,4 +1,28 @@
 <html>
+<head>
+    <style>
+        form {
+            display: flex;
+        }
+        input[type="submit"] {
+            width: 100px;
+        }
+        input {
+            margin: 10px;
+        }
+    </style>
+</head>
 <body>
+    <form >
+        <div>
+            <input type="number" name="numX" >
+        </div>
+        <div>
+            <input type="number" name="numY" >
+        </div>
+        <div>
+            <input type="submit" value="=" >
+        </div>
+    </form>
 </body>
 </html>
