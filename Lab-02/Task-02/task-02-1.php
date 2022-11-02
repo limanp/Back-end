b36bd90a5e0b2fb6757acda85da30eee477e59a6
limanp <pasha.liman@ukr.net>
2022-11-02 16:45:36 +0200

    Minor edits for createTable

--- Lab-02/Task-02/task-02-1.php
+++ Lab-02/Task-02/task-02-1.php
@@ -1,4 +1,19 @@
 <html>
+<head>
+    <style>
+        td {
+            width: 300px;
+            height: 100px;
+            text-align: center;
+        }
+        table {
+            border-collapse: collapse;
+        }
+        table, td {
+            border: black solid 3px;
+        }
+    </style>
+</head>
 <body>
 <?php
  echo createTable(5, 4);
@@ -13,7 +28,10 @@ function createTable($row, $col)
     for($i = 0; $i < $row; $i++) {
         $table .= '<tr>';
         for($j = 0; $j < $col; $j++) {
-            $table .= '<td>777</td>';
+            $red = rand(0, 255);
+            $green = rand(0, 255);
+            $blue = rand(0, 255);
+            $table .= "<td style='background-color:rgb($red, $green, $blue);' >$red $green $blue</td>";
         }
         $table .= '</tr>';
     }
