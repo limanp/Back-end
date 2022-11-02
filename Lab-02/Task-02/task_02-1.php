7debedc8a1fbc72e3cb5a4e69ac7e489e94cfb87
limanp <pasha.liman@ukr.net>
2022-11-02 17:34:00 +0200

    add function createSquare

--- Lab-02/Task-02/task_02-1.php
+++ Lab-02/Task-02/task_02-1.php
@@ -1,5 +1,36 @@
 <html>
+<head>
+    <style>
+        body {
+            background-color: black;
+        }
+    </style>
+</head>
 <body>
-
+<div>
+    <?php
+        echo createSquare(10);
+    ?>
+</div>
 </body>
 </html>
+
+<?php
+function createSquare($quantity)
+{
+    $squares = '<div style:"position: relative">';
+    $counter = 0;
+    while ($counter < $quantity) {
+        $size = rand(20, 200);
+        $marginLeft = rand(0, 1200);
+        $marginRight = rand(0, 700);
+        $marginTop = rand(0, 700);
+        $squares .= "<div style='background-color: red; width:$size; height: $size;
+        margin-left:$marginLeft; margin-right:$marginRight; margin-top:$marginTop; position:absolute' ></div>";
+        $counter++;
+    }
+    return $squares .= '</div>';
+}
+
+?>
+
