7a55a74ade6c3367ea2194056a142ec734f95100
limanp <pasha.liman@ukr.net>
2022-11-02 09:38:57 +0200

    added createTable function

--- Lab-02/Task-02/task-02-1.php
+++ Lab-02/Task-02/task-02-1.php
@@ -1,12 +1,23 @@
 <html>
 <body>
 <?php
-
+ echo createTable(5, 4);
 ?>
 </body>
 </html>
 
 <?php
-
+function createTable($row, $col)
+{
+    $table = '<table>';
+    for($i = 0; $i < $row; $i++) {
+        $table .= '<tr>';
+        for($j = 0; $j < $col; $j++) {
+            $table .= '<td>777</td>';
+        }
+        $table .= '</tr>';
+    }
+    return $table .= '</table>';
+}
 ?>
 
