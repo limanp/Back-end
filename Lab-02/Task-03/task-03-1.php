3ff06544fa142c31ebf6dab98d221b21a2ae764d
limanp <pasha.liman@ukr.net>
2022-11-03 16:21:13 +0200

    lab-2 task-3-1 finish

--- Lab-02/Task-03/task-03-1.php
+++ Lab-02/Task-03/task-03-1.php
@@ -1,5 +1,38 @@
 <html>
 <body>
-
+<?php
+$numbers = [5, 2, 4, 5, 1, 2, 4, 7, 4, 6, 4, 0, 0];
+$repeatNumbers = findRepeatNumbers($numbers);
+print_r($numbers);
+echo '<br>';
+print_r($repeatNumbers);
+?>
 </body>
 </html>
+
+<?php
+function findRepeatNumbers($numbers)
+{
+    $repeatNumbers = array();
+    $isNumberInArray = false;
+    for ($i = 0; $i < count($numbers) - 1; $i++) {
+        $count = 0;
+        for ($j = 0; $j < count($numbers); $j++) {
+            if ($numbers[$i] === $numbers[$j]) {
+                $number = $numbers[$i];
+                $count++;
+            }
+        }
+        for ($j = 0; $j < count($repeatNumbers); $j++) {
+            $isNumberInArray = $number === $repeatNumbers[$j] ? true : false;
+            if ($isNumberInArray)
+                break;
+        }
+        if (!$isNumberInArray && $count > 1)
+            $repeatNumbers[] = $number;
+        $count = 0;
+    }
+    return $repeatNumbers;
+}
+
+?>
