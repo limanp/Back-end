177d950a4ef7825e98c831c63a9b06b07c896f81
limanp <pasha.liman@ukr.net>
2022-11-04 13:14:38 +0200

    create arrayMerge Function

--- Lab-02/Task-03/task-03-3.php
+++ Lab-02/Task-03/task-03-3.php
@@ -1,18 +1,39 @@
 <html>
 <body>
-
+<?php
+print_r(arrayCreate());
+?>
 </body>
 </html>
 
 <?php
-function createArray()
+
+// Для зливання масивів є array_merge() але я написав свій варіант коду
+function arrayMerge($arr1, $arr2): array
+{
+    $result = array();
+    foreach ($arr1 as $value)
+        $result[] = $value;
+    foreach ($arr2 as $value)
+        $result[] = $value;
+    return $result;
+}
+
+function arrayEditing($arr1, $arr2): array
+{
+    $arrayMerged = arrayMerge($arr1, $arr2);
+
+}
+
+// Функція для сторювання масиву
+function arrayCreate(): array
 {
     $size = rand(3, 7);
-    $numbers = [];
-    for($i = 0; $i < $size; $i++) {
-        $numbers[] = rand(10, 20);
+    $arr = array();
+    for ($i = 0; $i < $size; $i++) {
+        $arr[] = rand(10, 20);
     }
-    return $numbers;
+    return $arr;
 }
 
 ?>
