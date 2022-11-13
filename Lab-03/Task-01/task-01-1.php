b106c813898c566eb03a9b182b1dcd73370d98eb
limanp <pasha.liman@ukr.net>
2022-11-13 12:06:44 +0200

    created form

---
+++ Lab-03/Task-01/task-01-1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <style>
+        .inputs {
+            width: 200px;
+        }
+        .inputs input {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+<form>
+    <div class="inputs">
+        <input name="text" placeholder="текст">
+        <input name="find" placeholder="знайти">
+        <input name="result" placeholder="результат">
+    </div>
+</form>
+</body>
+</html>
+
