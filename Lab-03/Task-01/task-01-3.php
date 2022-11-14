98877ad7064204264f6a7ea13a34c3cb14363743
limanp <pasha.liman@ukr.net>
2022-11-14 16:18:21 +0200

    created function findFileName

---
+++ Lab-03/Task-01/task-01-3.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task-01-3</title>
+</head>
+<body>
+<?php
+$filePath = 'D:\WebServers\home\testsite\www\myfile.txt';
+$fileName = findFileName($filePath, '\\');
+echo $fileName;
+?>
+</body>
+</html>
+
+<?php
+function findFileName($filePath, $separator)
+{
+    $position = mb_strrpos($filePath, $separator) + 1;
+    return mb_substr($filePath, $position);
+}
+?>
