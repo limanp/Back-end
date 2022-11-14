be0ecc7ae18edc3055cc684998963c4f800681e4
limanp <pasha.liman@ukr.net>
2022-11-14 13:31:24 +0200

    task-01-1 done

--- Lab-03/Task-01/task-01-1.php
+++ Lab-03/Task-01/task-01-1.php
@@ -1,9 +1,16 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task-01-1</title>
     <style>
         .inputs {
             width: 200px;
         }
+
         .inputs input {
             margin-bottom: 20px;
         }
@@ -15,9 +22,16 @@
         <input name="text" placeholder="текст">
         <input name="find" placeholder="знайти">
         <input name="replace" placeholder="замінити">
-        <input name="result" placeholder="результат">
+        <?php
+        $text = $_GET['text'];
+        $oldString = $_GET['find'];
+        $newString = $_GET['replace'];
+
+        $result = str_replace($oldString, $newString, $text);
+        echo "<input name='result' placeholder='результат' value=\"$result\" >";
+        ?>
+        <input type="submit">
     </div>
 </form>
 </body>
 </html>
-
