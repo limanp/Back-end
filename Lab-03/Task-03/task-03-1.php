451fc807ed93bb8c09719e7a5d77d4ddcaf7ca21
limanp <pasha.liman@ukr.net>
2022-11-18 20:18:32 +0200

    write in file

--- Lab-03/Task-03/task-03-1.php
+++ Lab-03/Task-03/task-03-1.php
@@ -17,9 +17,28 @@
 </head>
 <body>
     <form>
-        <input name="name" placeholder="Ім'я">
-        <textarea name="comment" placeholder="Коментар"></textarea>
+        <label>
+            <input name="name" placeholder="Ім'я">
+            <textarea name="comment" placeholder="Коментар" maxlength="100"></textarea>
+        </label>
         <input type="submit">
     </form>
+    <table>
+        <?php
+
+        ?>
+    </table>
 </body>
 </html>
+
+<?php
+$name = $_GET["name"];
+$comment = $_GET["comment"];
+
+$commentsFile = fopen("comments.txt", 'a+') or die("Не вдалося відкрити файл");
+fseek($commentsFile, 0, SEEK_END);
+fwrite($commentsFile, "$name : $comment\n");
+fclose($commentsFile);
+echo $name;
+
+?>
