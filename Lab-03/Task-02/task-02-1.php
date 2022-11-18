97e80c2baa20ab8b555393c9aa0eaa6caf99cf52
limanp <pasha.liman@ukr.net>
2022-11-18 12:58:00 +0200

    create label and array months

--- Lab-03/Task-02/task-02-1.php
+++ Lab-03/Task-02/task-02-1.php
@@ -8,6 +8,21 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+$month = ["01" => "Січень", "02" => "Лютий", "03" => "Березень", "04" => "Квітень", "05" => "Травень",
+    "06" => "Червень", "07" => "Липень", "08" => "Серпень", "09" => "Вересень", "10" => "Жовтень",
+    "11" => "Листопад", "12" => "Грудень",];
+?>
+<label>
+    <select name="number">
+        <option value="1" selected>1</option>
+    </select>
+    <select name="month">
 
+    </select name="year">
+    <select>
+
+    </select>
+</label>
 </body>
 </html>
