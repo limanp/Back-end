ece76dcbf5bd173bb007eabfd99e16cebe15f8d7
limanp <pasha.liman@ukr.net>
2022-11-18 19:22:35 +0200

    created form

--- Lab-03/Task-03/task-03-1.php
+++ Lab-03/Task-03/task-03-1.php
@@ -1,13 +1,25 @@
 <!doctype html>
-<html lang="en">
+<html lang="uk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Task-03-1</title>
+    <style>
+        form {
+            width: 250px;
+        }
+        input{
+            margin-bottom: 5px;
+        }
+    </style>
 </head>
 <body>
-
+    <form>
+        <input name="name" placeholder="Ім'я">
+        <textarea name="comment" placeholder="Коментар"></textarea>
+        <input type="submit">
+    </form>
 </body>
 </html>
