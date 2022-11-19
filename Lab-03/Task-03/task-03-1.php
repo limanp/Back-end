17282642ccd9c757e91d351eaea9f541ee3f224c
limanp <pasha.liman@ukr.net>
2022-11-19 21:47:04 +0200

    Lab-03 task-03-1 done

--- Lab-03/Task-03/task-03-1.php
+++ Lab-03/Task-03/task-03-1.php
@@ -13,6 +13,15 @@
         input{
             margin-bottom: 5px;
         }
+        table, th, td {
+            border: 1px solid white;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 20px;
+            background-color: black;
+            color: white;
+        }
     </style>
 </head>
 <body>
@@ -24,7 +33,25 @@
         <input type="submit">
     </form>
     <table>
+        <tr><th>Ім'я</th><th>Коментар</th></tr>
         <?php
+        $name = $_GET["name"];
+        $comment = $_GET["comment"];
+        // Можна зробити набагато проще просто через параметр а, але хотів використати функцію fseek
+        $mode = file_exists("comments.txt") ? 'r+' : 'w+';
+        $commentsFile = fopen("comments.txt", $mode) or die("Не вдалося відкрити файл");
+
+        if(!is_null($name) && !is_null($comment) && $name != "" && $comment != "")
+        {
+            fseek($commentsFile, 0, SEEK_END);
+            fwrite($commentsFile, "$name:$comment\n");
+        }
+        fseek($commentsFile, 0);
+        while(!feof($commentsFile)) {
+            $line = fgets($commentsFile);
+            lineToTable($line);
+        }
+        fclose($commentsFile);
 
         ?>
     </table>
@@ -32,13 +59,12 @@
 </html>
 
 <?php
-$name = $_GET["name"];
-$comment = $_GET["comment"];
-
-$commentsFile = fopen("comments.txt", 'a+') or die("Не вдалося відкрити файл");
-fseek($commentsFile, 0, SEEK_END);
-fwrite($commentsFile, "$name : $comment\n");
-fclose($commentsFile);
-echo $name;
-
+function lineToTable($line)
+{
+    $pos = mb_strpos($line, ":");
+    $name = mb_substr($line, 0, $pos);
+    $comment = mb_substr($line, $pos + 1);
+    if($name != "" && $comment != "")
+        echo "<tr><td>$name</td><td>$comment</td></tr>";
+}
 ?>
