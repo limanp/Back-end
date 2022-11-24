4343ff90efcb5bf310aa1556c74cd0733429a6f2
limanp <pasha.liman@ukr.net>
2022-11-24 23:32:41 +0200

    corrections item one and done item two

--- Lab-03/Task-03/task-03-2/index.php
+++ Lab-03/Task-03/task-03-2/index.php
@@ -10,8 +10,9 @@
 <body>
 <?php
 
-function wordsOnlyFirst($words1, $words2) : string
+function wordsOnlyFirstFile($words1, $words2)
 {
+    /*
     $result = "";
     $isMatch = false;
     foreach($words1 as $value1) {
@@ -25,17 +26,31 @@ function wordsOnlyFirst($words1, $words2) : string
 
     }
     return $result;
+    */
+    $wordsOnlyFirstFile = fopen("result/wordsOnlyFirstFile.txt", "w") or die("Не вдалося відкрити файл");
+    foreach(array_diff($words1, $words2) as $value) {
+        fwrite($wordsOnlyFirstFile, $value . " ");
+    }
+    fclose($wordsOnlyFirstFile);
+}
+function wordsBothFile($words1, $words2)
+{
+    $wordsBothFile = fopen("result/wordsBothFile.txt", "w") or die("Не вдалося відкрити файл");
+    foreach(array_intersect($words1, $words2) as $value) {
+        fwrite($wordsBothFile, $value . " ");
+    }
+    fclose($wordsBothFile);
 }
 
 $wordsStr = file_get_contents("words/words1.txt");
-$wordsArr1 = explode(" ", $wordsStr);
+$fileWords1 = explode(" ", $wordsStr);
 
 $wordsStr = file_get_contents("words/words2.txt");
-$wordsArr2 = explode(" ", $wordsStr);
+$fileWords2 = explode(" ", $wordsStr);
 
-$wordsOnlyFirstFile = fopen("result/OnlyFirstFile.txt", "w") or die("Не вдалося відкрити файл");
-fwrite($wordsOnlyFirstFile, wordsOnlyFirst($wordsArr1, $wordsArr2));
-fclose($wordsOnlyFirstFile);
+
+wordsOnlyFirstFile($fileWords1, $fileWords2);
+wordsBothFile($fileWords1, $fileWords2);
 
 
 ?>
